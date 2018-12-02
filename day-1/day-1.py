5b8583cc0b642c60b3ca1515cd89ed59cc49808b
Zachery Gentry <zacherygentry@gmail.com>
2018-12-01 22:16:33 -0600

    part 2 with counters

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,8 +1,39 @@
-f = open("input.txt", "r")
+import time
 
-sum = 0
 
-for line in f:
-    sum += int(line)
+def part1():
+    f = open("input.txt", "r")
 
-print(sum)
+    sum = 0
+
+    for line in f:
+        sum += int(line)
+
+    print(sum)
+    f.close()
+
+
+def part2():
+    counter = 0
+    tic = time.clock()
+    sum = 0
+    arr = [0]
+    while True:
+        f = open("input.txt", "r")
+        for line in f:
+            num = int(line)
+            sum += num
+            if sum in arr:
+                print(sum)
+                toc = time.clock()
+                print(toc - tic)
+                return
+            else:
+                arr.append(sum)
+        counter += 1
+        print("Iterated " + str(counter) + " times")
+        f.close()
+
+
+part1()
+part2()
